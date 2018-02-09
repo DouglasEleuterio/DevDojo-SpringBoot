1557a60e01d03b29ddeb234c554a04702c72e561
DouglasEleuterio <douglas.versato@gmail.com>
2018-02-08 22:07:23 -0200

    Substituição das Anotações @Configuration @ComponentScan @EnableAutoConfiguration
    Pela anotação
    @SpringBootApplication
    Mantendo a mesma funcionalidade

---
+++ src/main/java/br/com/devdojo/awesome/ApplicationStart.java
@@ -0,0 +1,16 @@
+package br.com.devdojo.awesome;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication // @EnableAutoConfiguration, @Configuration, @ComponentScan
+
+public class ApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class,args);
+    }
+}
